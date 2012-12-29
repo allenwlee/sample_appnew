ba31c8cce55c0b63e7cbeba21a5a1d5af43adf38
Allen Lee <allenwlee130@gmail.com>
2012-12-29 11:11:15 -0800

    totally late initial bitches

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '84d73f2e7ca59de217737769a77749a00629fe0dc7a51015899386c583f115023a136e62d4e5dd204a8e54dd5bf6d9285d016e1d7626e9d8a2214c971ea2d10d'
