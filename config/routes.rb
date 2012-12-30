5f736591279bef1bc68c4f9615bc39fc09c0b6c4
Allen Lee <allenwlee130@gmail.com>
2012-12-29 16:52:41 -0800

    railstutorial chp 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 SampleApp::Application.routes.draw do
   
   resources :users
-  root to: 'static_pages#home'
+  resources :sessions, only: [:new, :create, :destroy]
+
+  root to: 'users#new'
   match '/signup', to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   get "static_pages/home"
   get "static_pages/about"
